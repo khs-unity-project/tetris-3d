ca257a23e478b467e87d2ab9d4fd6be5b54df888
Blr-Mac <44504005+Blr-Mac@users.noreply.github.com>
2019-04-11 08:39:30 -0700

    a
    a

---
+++ tetris-3d/Assets/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject borderPrefab; 
+
+
+	// Use this for initialization
+	void Start () {
+        GameObject wallPiece = Instantiate(borderPrefab);
+        wallPiece.transform.position = new Vector3(
+            0,
+            0,
+            0
+            );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
