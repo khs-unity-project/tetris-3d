4548c6e63eacb3fafcbf3ce047c82011e87c0707
Blr-Mac <44504005+Blr-Mac@users.noreply.github.com>
2019-04-15 08:23:09 -0700

    trtrttt
    tt

--- tetris-3d/Assets/CameraController.cs
+++ tetris-3d/Assets/CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 
     public GameObject[] gameCameras;
 
