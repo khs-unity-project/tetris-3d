a206693669f774615cf732d409f33133ceb6274e
BdnMac <44503987+BdnMac@users.noreply.github.com>
2019-04-17 08:01:17 -0700

    baylor bs

--- tetris-3d/Assets/blockMove.cs
+++ tetris-3d/Assets/blockMove.cs
@@ -4,57 +4,72 @@ using UnityEngine;
 
 public class blockMove : MonoBehaviour {
 
+    public GameObject blockPrefab;
 
+
+    public float fallSpeed = 1;
     public float speed = 3.5f;
     public float rotatingSpeed = 140f;
+    public bool blockCollide = false;
+
     // Use this for initialization
     void Start () {
-		
+        blockCollide = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKey("s"))
+        if (blockCollide == false)
         {
-            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime);
-        }
+
+            if (Input.GetKey("down"))
+            {
+                transform.Translate(0, -speed * Time.deltaTime, 0, Space.World);
+            }
+            if (Input.GetKey("right"))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0, Space.World);
+            }
+            if (Input.GetKey("left"))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0, Space.World);
+            }
+
+            transform.Translate(0, -fallSpeed * Time.deltaTime, 0, Space.World);
 
 
 
-        if (Input.GetKey("down"))
-        {
-            transform.Translate(0, -speed * Time.deltaTime, 0, Space.World);
-        }
-        if (Input.GetKey("right"))
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0, Space.World);
-        }
-        if (Input.GetKey("left"))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0, Space.World);
+            if (Input.GetKey("z"))
+            {
+                transform.RotateAround(transform.position, Vector3.forward, rotatingSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKey("c"))
+            {
+                transform.RotateAround(transform.position, Vector3.forward, -rotatingSpeed * Time.deltaTime);
+            }
+
         }
 
 
-        if (Input.GetKey("z"))
-        {
-            transform.RotateAround(transform.position, Vector3.forward, rotatingSpeed * Time.deltaTime);
-        }
+        
 
-        if (Input.GetKey("c"))
-        {
-            transform.RotateAround(transform.position, Vector3.forward, -rotatingSpeed * Time.deltaTime);
-        }
 
-        if (Input.GetKey("a"))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0, Space.World);
-        }
-
-        if (Input.GetKey("d"))
-        {
-            transform.Translate(speed * Time.deltaTime, 0, 0, Space.World);
-        }
 
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "TriggerExplosion")
+        {
+            blockCollide = true;
+
+            GameObject blockObject = Instantiate(blockPrefab);
+            //   blockObject.transform.position = transform.TransformPoint(0,10,0);
+            blockObject.transform.position = new Vector3(0,10,0);
+        }
+    }
+
+
 }
