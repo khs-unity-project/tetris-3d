abc632b8ebe1d25ef07142bfe80dc50216173e55
Blr-Mac <44504005+Blr-Mac@users.noreply.github.com>
2019-04-25 08:38:13 -0700

    v
    v

--- tetris-3d/Assets/CheckerController.cs
+++ tetris-3d/Assets/CheckerController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class CheckerController : MonoBehaviour {
     public GameObject checkerPrefab;
+    public int cxpos;
+    public int cypos;
     // Use this for initialization
     void Start () {
-        //for (int i = 0; i < 2; i++)
-        //{
-            GameObject checkerPiece = Instantiate(checkerPrefab);
-            checkerPiece.transform.position = new Vector3(
-                1,
-                2,
-                0
-                );
-        //}
+
+        
+        for (int i = 0; i < 23; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                GameObject checkerPiece = Instantiate(checkerPrefab);
+                checkerPiece.transform.position = new Vector3(
+                    cxpos + j + 1,
+                    cypos + i + 2,
+                    1
+                    );
+            }
+        }
     }
 	
 	// Update is called once per frame
